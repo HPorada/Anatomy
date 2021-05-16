93e93c18bfde81b686a548c34dd2f1d490ecc14e
HPorada <75125104+HPorada@users.noreply.github.com>
2021-05-17 00:26:47 +0200

    The current version

---
+++ anatomy/src/main/java/com/project/anatomy/AnatomyApplication.java
@@ -0,0 +1,13 @@
+package com.project.anatomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnatomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnatomyApplication.class, args);
+    }
+
+}
---
+++ anatomy/src/main/java/com/project/anatomy/configuration/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.project.anatomy.configuration;
+
+public class SecurityConfig {
+}
