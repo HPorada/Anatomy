6158623dd61f6e1d9c347e4a5e2c1f2ab8971e4a
soresu1520 <theluckyone116@gmail.com>
2021-05-17 11:56:04 +0200

    Services

--- src/main/java/com/project/anatomy/service/AnswerManager.java
+++ src/main/java/com/project/anatomy/service/AnswerManager.java
@@ -1,7 +1,27 @@
 package com.project.anatomy.service;
 
+import com.project.anatomy.repository.AnswerRepository;
+import com.project.anatomy.repository.entity.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AnswerManager {
+
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    public AnswerManager(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    public Iterable<Answer> findAll(){
+        return answerRepository.findAll();
+    }
+
+    public Optional<Answer> findById(Long id){
+        return answerRepository.findById(id);
+    }
 }
--- src/main/java/com/project/anatomy/service/UserManager.java
+++ src/main/java/com/project/anatomy/service/UserManager.java
@@ -1,7 +1,29 @@
 package com.project.anatomy.service;
 
+import com.project.anatomy.repository.AnswerRepository;
+import com.project.anatomy.repository.UserRepository;
+import com.project.anatomy.repository.entity.Answer;
+import com.project.anatomy.repository.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserManager {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserManager(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Iterable<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
 }
